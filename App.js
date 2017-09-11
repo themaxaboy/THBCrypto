ec61e68608d078e19c14817957648842c7a3e9f2
Themaxaboy <themaxaboy@hotmail.com>
2017-09-12 00:29:01 +0700

    format code

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import HomeScreen from './screens/HomeScreen';
-import RootNavigation from './navigation/RootNavigation';
+import React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { Expo, AppLoading, Asset, Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screens/HomeScreen";
+import RootNavigation from "./navigation/RootNavigation";
 
 export default class App extends React.Component {
-  state = {
-    assetsAreLoaded: false,
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      assetsAreLoaded: false
+    };
+  }
   async componentWillMount() {
     this._loadAssetsAsync();
   }
@@ -20,8 +22,10 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
-          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+          {Platform.OS === "ios" && <StatusBar barStyle="light-content" />}
+          {Platform.OS === "android" && (
+            <View style={styles.statusBarUnderlay} />
+          )}
           <HomeScreen />
         </View>
       );
@@ -30,29 +34,26 @@ export default class App extends React.Component {
 
   async _loadAssetsAsync() {
     await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
     });
-    
     try {
       await Promise.all([
-        Asset.loadAsync([
-          require('./assets/images/app-icon.png')
-        ]),
+        Asset.loadAsync([require("./assets/images/app-icon.png")]),
         Font.loadAsync([
           // This is the font that we are using for our tab bar
           Ionicons.font
           // We include SpaceMono because we use it in HomeScreen.js. Feel free
           // to remove this if you are not using it in your app
-          //{ 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-        ]),
+          // { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        ])
       ]);
     } catch (e) {
       // In this case, you might want to report the error to your error
       // reporting service, for example Sentry
       console.warn(
-        'There was an error caching assets (see: App.js), perhaps due to a ' +
-        'network timeout, so we skipped caching. Reload the app to try again.'
+        "There was an error caching assets (see: App.js), perhaps due to a " +
+          "network timeout, so we skipped caching. Reload the app to try again."
       );
       console.log(e);
     } finally {
@@ -64,10 +65,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff'
+    backgroundColor: "#ffffff"
   },
   statusBarUnderlay: {
     height: 24,
-    backgroundColor: '#53bcf3'
-  },
+    backgroundColor: "#53bcf3"
+  }
 });
